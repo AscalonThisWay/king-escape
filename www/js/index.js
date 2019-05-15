4bc2c15a2edb11b4fb7f655e7a8e4d4aba3563af
Maxime Genaudet <maxime.genaudet@soprabanking.com>
2019-05-15 13:57:06 +0200

    Added Phaser Canvas and a sample Grid

--- www/js/index.js
+++ www/js/index.js
@@ -16,31 +16,28 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+document.addEventListener('deviceready', function() {
+    var config = {
+        type: Phaser.WEBGL,
+        parent: 'game',
+        width: window.innerWidth * window.devicePixelRatio,
+        height: window.innerHeight * window.devicePixelRatio,
+        scene: {
+            preload: preload,
+            create: create
+        }
+    };
+    
+    var game = new Phaser.Game(config);
+    
+    function preload() {
     }
-};
+    
+    function create() {
+        var g2 = this.add.grid(300, 340, 512, 256, 64, 64, 0x00b9f2).setAltFillStyle(0x016fce).setOutlineStyle();
+    }    
+});
 
-app.initialize();
+if (!window.cordova) {
+    window.dispatchEvent('deviceready');
+}
